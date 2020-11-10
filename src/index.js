c70b0c8355e61785a446f12d4041951e11ee6005
Sebastian Alexandersson <sebastian.alexandersson@bilvision.se>
2020-11-10 19:36:06 +0100

    Added class Course

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const Course = require('./Classes/Course');
+
+const javascript = new Course({
+  id: 1,
+  students: ['Pelle', 'Kalle', 'Adam'],
+  teacher: 'John Smith',
+  startDate: new Date(Date.now()),
+  endDate: new Date(Date.now() + (3600 * 60 * 24 * 7)),
+})
+
+console.log(javascript);
+
+javascript.addStudent('John Doe');
+javascript.removeStudent('Brad Pitt');
+javascript.removeStudent('Pelle');
